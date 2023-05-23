50a870feefae7d386294763ca088b0be74b37b36
Claire Lin <chunhuilin7516@gmail.com>
2023-05-23 17:50:17 +0200

    Add tests for showing, updating and destroying a quote

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -1,11 +1,11 @@
 require "application_system_test_case"
 
 class QuotesTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit quotes_url
-  #
-  #   assert_selector "h1", text: "Quotes"
-  # end
+  setup do
+    @quote = quotes(:first) # Reference to the first fixture quote
+  end
+
+
   test "Creating a new quote" do
     # 1. When we visit the Quotes#index page
     # we expect to see a title with the text Quotes
@@ -13,7 +13,7 @@ class QuotesTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Quotes"
 
     # 2. When we click on the link with the text "New quote"
-    # we expect to go to Quotes#new page with athe title "New quote"
+    # we expect to go to Quotes#new page with the title "New quote"
     click_on "New quote"
     assert_selector "h1", text: "New quote"
 
@@ -26,5 +26,34 @@ class QuotesTest < ApplicationSystemTestCase
     # and to see our "Capybara quote" added to the list
     assert_selector "h1", text: "Quotes"
     assert_text "Capybara quote"
+  end
+
+  test "Showing a quote" do
+    visit quotes_path
+    click_link @quote.name
+
+    assert_selector "h1", text: @quote.name
+  end
+
+  test "Updating a quote" do
+    visit quotes_path
+    assert_selector "h1", text: "Quotes"
+
+    click_on "Edit", match: :first
+    assert_selector "h1", text: "Edit quote"
+
+    fill_in "Name", with: "Updated quote"
+    click_on "Update quote"
+
+    assert_selector "h1", text: "Quotes"
+    assert_text "Updated quote"
+  end
+
+  test "Destroying a quote" do
+    visit quotes_path
+    assert_text @quote.name
+
+    click_on "Delete", match: :first
+    assert_no_text @quote.name
   end
 end
